ce2e7169daf0f963d282a32ad5c7c4d6cb219e86
Alexandra Harley <aharleydev@gmail.com>
2022-05-06 19:06:57 -0400

    updated CSS design and added media queries

--- js/main.js
+++ js/main.js
@@ -12,18 +12,20 @@ function findDaily() {
             console.log(data.date)
             console.log(data.title)
             console.log(data.hdurl)
-            document.querySelector('.title').innerText = 'Photo Title: ' + data.title
-            document.querySelector('.description').innerText = 'Photo Description: ' + data.explanation
+            document.querySelector('.title').innerText = 'Photo Title:  ' + data.title
+            document.querySelector('.description').innerText = 'Photo Description:  ' + data.explanation
             if (data.media_type === 'image'){
-                document.querySelector('img').src = data.hdurl
-                document.querySelector('img').classList.remove('hidden')
+                document.querySelector('.space').src = data.hdurl
+                document.querySelector('.space').classList.remove('hidden')
                 document.querySelector('iframe').classList.add('hidden')
             } else if (data.media_type === 'video') {
-                document.querySelector('img').classList.add('hidden')
+                document.querySelector('.space').classList.add('hidden')
                 document.querySelector('iframe').src = data.url
                 document.querySelector('iframe').classList.remove('hidden')
             } else {
                 alert('Unsupported Media Type')
+                document.querySelector('iframe').classList.add('hidden')
+                document.querySelector('.space').classList.add('hidden')
             }
         })
         .catch(err => {
